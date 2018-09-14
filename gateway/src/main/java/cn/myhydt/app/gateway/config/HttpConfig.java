af75827db050ba18b2c14b2f822762d5039bf782
hy9902 <hy9902@sina.com>
2018-09-14 16:34:05 +0800

    更新spring-boot-parent;

--- gateway/src/main/java/cn/myhydt/app/gateway/config/HttpConfig.java
+++ gateway/src/main/java/cn/myhydt/app/gateway/config/HttpConfig.java
@@ -1,7 +1,12 @@
 package cn.myhydt.app.gateway.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,8 +23,20 @@ public class HttpConfig {
         return builder.build();
     }*/
 
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
     @Bean
-    RedisRateLimiter redisRateLimiter() {
+    public RedisRateLimiter redisRateLimiter() {
         return new RedisRateLimiter(100, 200);
     }
+
+    @Bean
+    public RouteLocator customRouteLocator(){
+        RouteLocatorBuilder.Builder routeLocatorBuilder = new RouteLocatorBuilder(applicationContext).routes();
+        routeLocatorBuilder.route("path_route", t -> t.path("/path").uri("http://"))
+                .route("", t -> t.header("").uri(""));
+        return routeLocatorBuilder.build();
+    }
+
 }
