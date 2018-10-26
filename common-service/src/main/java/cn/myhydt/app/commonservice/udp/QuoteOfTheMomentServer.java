252579329bf8b81b091907202846cee54f5fc74e
hy9902 <hy9902@sina.com>
2018-10-26 17:24:56 +0800

    update

---
+++ common-service/src/main/java/cn/myhydt/app/commonservice/udp/QuoteOfTheMomentServer.java
@@ -0,0 +1,31 @@
+package cn.myhydt.app.commonservice.udp;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+
+/**
+ * @author hy9902
+ * @create 2018-10-26 15:42
+ */
+public final class QuoteOfTheMomentServer {
+
+    private static final int PORT = Integer.parseInt(System.getProperty("port", "7686"));
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .option(ChannelOption.SO_BROADCAST, true)
+                    .handler(new QuoteOfTheMomentServerHandler());
+
+            b.bind(PORT).sync().channel().closeFuture().await();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
